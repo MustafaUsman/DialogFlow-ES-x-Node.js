5d643c2a0db345e4ed91240d44ab5ed5595833ff
Mustafa Usman <mu06166@st.habib.edu.pk>
2023-06-01 16:06:13 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,54 +9,68 @@ router.post('/', function (req, res, next) {
   let orderid = req.body.queryResult.parameters.orderid;
   let intent = req.body.queryResult.intent.displayName;
 
+  if (orderid === '31313') {
+    // Print the order ID in the terminal
+    console.log('Order ID:', orderid);
 
-  // Print the order ID in the terminal
-  console.log('Order ID:', orderid);
+    // Make a POST request to the Order Status API
+    axios
+      .post('https://orderstatusapi-dot-organization-project-311520.uc.r.appspot.com/api/getOrderStatus', {
+        orderId: orderid
+      })
+      .then(function (response) {
+        // Extract the shipment date from the API response
+        let shipmentDate = response.data.shipmentDate;
+        let formattedDate = moment(shipmentDate).format('dddd, DD MMM YYYY');
+        console.log(formattedDate); // Output: Thursday, 18 Aug 2022
 
-  // Make a POST request to the Order Status API
-  axios.post('https://orderstatusapi-dot-organization-project-311520.uc.r.appspot.com/api/getOrderStatus', {
-    orderId: orderid
-  })
-    .then(function (response) {
-      // Extract the shipment date from the API response
-      let shipmentDate = response.data.shipmentDate;
-      let formattedDate = moment(shipmentDate).format('dddd, DD MMM YYYY');
-      console.log(formattedDate); // Output: Thursday, 18 Aug 2022
-
-      // Construct the response to be sent to Dialogflow
-      let responseJson = {
-        fulfillmentMessages: [
-          {
-            text: {
-              text: [`Your order with ID ${orderid} will be shipped on ${formattedDate}.`]
+        // Construct the response to be sent to Dialogflow
+        let responseJson = {
+          fulfillmentMessages: [
+            {
+              text: {
+                text: [`Your order with ID ${orderid} will be shipped on ${formattedDate}.`]
+              }
             }
-          }
-        ]
-      };
+          ]
+        };
 
-      // Send the response back to Dialogflow
-      res.send(responseJson);
-    })
-    
-    .catch(function (error) {
-      console.log(error);
-      // Handle any errors that occurred during the API request
+        // Send the response back to Dialogflow
+        res.send(responseJson);
+      })
+      .catch(function (error) {
+        console.log(error);
+        // Handle any errors that occurred during the API request
 
-      // Construct the response for error case
-      let responseJson = {
-        fulfillmentMessages: [
-          {
-            text: {
-              text: ['Sorry, there was an error retrieving the shipment date.']
+        // Construct the response for error case
+        let responseJson = {
+          fulfillmentMessages: [
+            {
+              text: {
+                text: ['Sorry, there was an error retrieving the shipment date.']
+              }
             }
-          }
-        ]
-      };
+          ]
+        };
 
-      // Send the response back to Dialogflow
-      res.send(responseJson);
-    });
-    
+        // Send the response back to Dialogflow
+        res.send(responseJson);
+      });
+  } else {
+    // Construct the response for invalid order ID
+    let responseJson = {
+      fulfillmentMessages: [
+        {
+          text: {
+            text: ['Invalid order ID. Please try again.']
+          }
+        }
+      ]
+    };
+
+    // Send the response back to Dialogflow
+    res.send(responseJson);
+  }
 });
 
 module.exports = router;
